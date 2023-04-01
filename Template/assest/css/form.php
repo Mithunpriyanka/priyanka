910227f92253146e0347312064ae34845ec58fbd
PRIYANKA G <128356494+Mithunpriyanka@users.noreply.github.com>
2023-04-01 00:24:17 -0930

    Add files via upload

---
+++ Template/assest/css/form.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<form name="sform" method="post" action="#" enctype="mulitipart_data">
+	Name:<input type="text" name="uname"/><br/>
+	Emailm id:<input type="email" name="email id"/><br/>
+	Mobile number:<input type="tel" name="mobile number"/><br/>
+	Gender:<input type="radio" name="gender" value="male"><label>male</label>
+	<input type="radio" name="gender" value="female"><label>female</label>
+	Date of Birth:<input type="date" name="udob" value="date"><br>
+	Adress:<textarea name="uaddress" row="5" cols="70"></textarea><br>
+	<form name="form1" method="post" action="#" enctype="multipart/form_date"></form>
+
+
+	
+	
+</form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
